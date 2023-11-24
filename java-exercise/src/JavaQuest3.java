86d27342dc4d5e523b44aca8902cf53172422eae
thomaskywong <thomaskywong@yahoo.com>
2023-11-25 06:09:24 +0800

    20231125 Quest3 revised

--- java-exercise/src/JavaQuest3.java
+++ java-exercise/src/JavaQuest3.java
@@ -21,8 +21,9 @@ public class JavaQuest3 {
     Scanner input = new Scanner(System.in);
 
     boolean isHeapYear = false;
-    final int[] DAY_OF_MONTH_NON_HEAP_YEAR =
+    final int[] DAYS_OF_MONTH =
         new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    final int DAYS_OF_FEB_HEAP_YEAR = 29;
 
     int number_Of_DaysInMonth = 0;
     String MonthOfName = "Unknown";
@@ -51,68 +52,69 @@ public class JavaQuest3 {
             .println("Invalid input! Please enter integer greater than 0.");
     }
 
-   if((year%4==0&&year%100!=0)||year%400==0)
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
       isHeapYear = true;
 
-  // Complete the switch statement
-  switch(month)
-  {
+    // Complete the switch statement
+    switch (month) {
       case 1:
         MonthOfName = "January";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       // Code here
       case 2:
         MonthOfName = "February";
         if (isHeapYear)
-          number_Of_DaysInMonth = 29;
+          number_Of_DaysInMonth = DAYS_OF_FEB_HEAP_YEAR;
         else
-          number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+          number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 3:
         MonthOfName = "March";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 4:
         MonthOfName = "April";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 5:
         MonthOfName = "May";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 6:
         MonthOfName = "June";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 7:
         MonthOfName = "July";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 8:
         MonthOfName = "Augest";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 9:
         MonthOfName = "September";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 10:
         MonthOfName = "October";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 11:
         MonthOfName = "November";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       case 12:
         MonthOfName = "Demember";
-        number_Of_DaysInMonth = DAY_OF_MONTH_NON_HEAP_YEAR[month - 1];
+        number_Of_DaysInMonth = DAYS_OF_MONTH[month - 1];
         break;
       default:
 
     }
 
-  System.out.print(MonthOfName+" "+year+" has "+number_Of_DaysInMonth+" days\n");
+    System.out.print(
+        MonthOfName + " " + year + " has " + number_Of_DaysInMonth + " days\n");
 
-}}
+  }
+}
