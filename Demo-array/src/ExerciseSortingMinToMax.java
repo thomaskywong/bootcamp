70809e8ec1c614c3870efad442d1a7665c7c4c78
thomaskywong <thomaskywong@yahoo.com>
2023-11-27 18:06:14 +0800

    SortingMinToMax, SortingMaxToMin

---
+++ Demo-array/src/ExerciseSortingMinToMax.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ExerciseSortingMinToMax {
+  public static void main(String[] args){
+
+    int[] arr2 = new int[] {100,-20, 4, 100, 500, -40, 68, 225, -1000};
+    int temp = Integer.MIN_VALUE;
+    
+    // result {500, 100, 100, 4, -20}
+    for (int i = 0; i < arr2.length -1; i++) {
+      for (int j = i + 1; j < arr2.length; j++ ) {
+        if (arr2[j] < arr2[i]) {
+          temp = arr2[i];
+          arr2[i] = arr2[j];
+          arr2[j] = temp; 
+        }
+      }
+    }
+
+    System.out.println(Arrays.toString(arr2));
+
+  }
+}
