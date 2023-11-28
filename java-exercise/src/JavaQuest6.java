0c235f755ef3b110dac4c123dfe3c13b2696d8ca
thomaskywong <thomaskywong@yahoo.com>
2023-11-28 18:21:46 +0800

    quest5 and quest6

---
+++ java-exercise/src/JavaQuest6.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class JavaQuest6 {
+  /**
+   * Expected output: 1 1 2 3 5 8 13 21 ...
+   * 
+   */
+  // Count the target of character in a String.
+  public static void main(String[] args) {
+    // for loop to print first 15 numbers in Fibonacci Sequence
+    int size = 15;
+    int[] seq = new int[size];
+    seq[0] = 1;
+    seq[1] = 1;
+
+    for (int i = 0; i < size; i++) {
+      if (i < 2) {
+        System.out.print(seq[i] + " ");
+        continue;
+      }
+      seq[i] = seq[i - 1] + seq[i - 2];
+      System.out.print(seq[i] + " ");
+    }
+
+    // System.out.println();
+    // System.out.println(Arrays.toString(seq));
+  }
+
+}
